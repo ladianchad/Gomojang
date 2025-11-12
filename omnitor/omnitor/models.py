32af7dc23d4c88430825f2874b5222f7a4576ff6
jueun-k1m <jueunkim.ee@gmail.com>
2025-11-12 11:05:56 +0900

    Add weight calibration fields to CalibrationSettings
    weight calibration <- change to ph calibration method

--- omnitor/omnitor/models.py
+++ omnitor/omnitor/models.py
@@ -50,8 +50,12 @@ class CalibrationSettings(models.Model):
     id = models.IntegerField(primary_key=True, default=1, editable=False)
 
     # Weight settings
-    weight_offset = models.FloatField(default=0.0)
-    weight_scale = models.FloatField(default=1.0)
+    weight_point1_raw = models.FloatField(default=20000)
+    weight_point1_value = models.FloatField(default=0)
+    weight_point2_raw = models.FloatField(default=40000)
+    weight_point2_value = models.FloatField(default=2000)
+    weight_slope = models.FloatField(default=-21.5)
+    weight_intercept = models.FloatField(default=20000)
     
     # pH two-point calibration data
     ph_point1_voltage = models.FloatField(default=2.5)
